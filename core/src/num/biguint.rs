5e2c451c0b4f6f88c688d0070df04045b46c9c8c
printfn <printfn@users.noreply.github.com>
2020-11-06 23:49:52 +1300

    Add u64 binary-gcd algorithm

--- core/src/num/biguint.rs
+++ core/src/num/biguint.rs
@@ -163,6 +163,49 @@ impl BigUint {
         Ok(a)
     }
 
+    // based on wikipedia
+    fn binary_gcd(mut u: u64, mut v: u64) -> u64 {
+        use std::cmp::min;
+        use std::mem::swap;
+
+        // Base cases: gcd(n, 0) = gcd(0, n) = n
+        if u == 0 {
+            return v;
+        } else if v == 0 {
+            return u;
+        }
+
+        // Using identities 2 and 3:
+        // gcd(2ⁱ u, 2ʲ v) = 2ᵏ gcd(u, v) with u, v odd and k = min(i, j)
+        // 2ᵏ is the greatest power of two that divides both u and v
+        let i = u.trailing_zeros();  u >>= i;
+        let j = v.trailing_zeros();  v >>= j;
+        let k = min(i, j);
+
+        loop {
+            // u and v are odd at the start of the loop
+            debug_assert!(u % 2 == 1, "u = {} is even", u);
+            debug_assert!(v % 2 == 1, "v = {} is even", v);
+
+            // Swap if necessary so u <= v
+            if u > v {
+                swap(&mut u, &mut v);
+            }
+
+            // Using identity 4 (gcd(u, v) = gcd(|v-u|, min(u, v))
+            v -= u;
+
+            // Identity 1: gcd(u, 0) = u
+            // The shift by k is necessary to add back the 2ᵏ factor that was removed before the loop
+            if v == 0 {
+                return u << k;
+            }
+
+            // Identity 3: gcd(u, 2ʲ v) = gcd(u, v) (u is known to be odd)
+            v >>= v.trailing_zeros();
+        }
+    }
+
     pub fn pow<I: Interrupt>(
         a: &Self,
         b: &Self,
